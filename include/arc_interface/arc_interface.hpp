06bb8fa0015b0d8e269650c88dd0e2709b39c471
buengern <buengern@student.ethz.ch>
2017-02-19 16:14:20 +0100

    new

--- include/arc_interface/arc_interface.hpp
+++ include/arc_interface/arc_interface.hpp
@@ -6,21 +6,63 @@
 #include "ros/ros.h"
 #include "ackermann_msgs/AckermannDrive.h"
 #include "std_msgs/Bool.h"
+#include "std_msgs/Float64.h"
+#include "arc_msgs/State.h"
 
 class interface {
  public:
  	interface(const ros::NodeHandle &nh);
   	void guardBoolCallback(const std_msgs::Bool::ConstPtr& msg);
   	void infoNiCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg);
- 	void ackCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg);
+ 	void CASCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg);
+ 	void stop_Callback(const arc_msgs::State::ConstPtr& msg);
+
+ 	// Methods for dspace Info (Black Box for types)
+ 	// void velocity_left_dspace_Callback(const std_msgs::Float64::ConstPtr& msg);
+	// void velocity_right_dspace_Callback(const std_msgs::Float64::ConstPtr& msg); 
+	// void steering_angle_dspace_Callback(const std_msgs::Float64::ConstPtr& msg); 
+
+
+
  	void kill();
+
  private:
-	ros::NodeHandle nh_; 	
+	ros::NodeHandle nh_; 
+
+	//Publisher
+	ros::Publisher ackermann_to_ni_pub_;
+	ros::Publisher steering_angle_pub_;
+	ros::Publisher steering_pub_;
+	ros::Publisher right_wheel_pub_;
+	ros::Publisher left_wheel_pub_;
+
+	// ros::Publisher left_wheel_dspace_sub;
+
+	//Subscriber for CAS
 	ros::Subscriber ackermann_sub_;
 	ros::Subscriber guard_bool_sub_;
+	//Subscriber for NI
 	ros::Subscriber sensors_ni_sub_;
-	ros::Publisher ackermann_to_ni_pub_;
+	//Subscriber for Dspace
+	ros::Subscriber dspace_left_wheel_sub_; //velocity left from Dspace
+	ros::Subscriber dspace_v_right_sub_; // velocity right from Dspace
+	//Subscriber for arc_msgs/State
+	ros::Subscriber emergency_stop_sub_;
+
+	//Msgs for CAS
 	ackermann_msgs::AckermannDrive control_input_;
-	ackermann_msgs::AckermannDrive sensors_ni_;
 	bool guard_bool_;
+
+	//Msgs for arc_msgs
+	bool stop_;
+
+	//Msgs from NI
+	ackermann_msgs::AckermannDrive sensors_ni_; //temporary ackermann_msgs = type of NI
+
+	//Msgs from Dspace
+	std_msgs::Float64 v_right_dspace_; //Suppose that the type of the single right velocity is Float64
+	std_msgs::Float64 v_left_dspace_;
+	std_msgs::Float64 steering_angle_dspace_;
+
+
  };
--- src/arc_interface.cpp
+++ src/arc_interface.cpp
@@ -9,25 +9,29 @@
 interface::interface(const ros::NodeHandle &nh) : nh_(nh)
 {
 	std::cout<<"I'm open"<<std::endl;
-	std::cout<<"CONNECTING WITH EMERGENCY.."<<std::endl;
 	guard_bool_sub_= nh_.subscribe("/not_stop", 10, &interface::guardBoolCallback, this);
-	std::cout<<"CHECK"<<std::endl;
-	std::cout<<"GETTING CONTROL INPUTS.."<<std::endl;
-	ackermann_sub_= nh_.subscribe("/stellgroessen_safe", 10, &interface::ackCallback, this);
-	std::cout<<"CHECK"<<std::endl;	
-	std::cout<<"CONNECTING WITH .."<<std::endl;	
-	ackermann_sub_= nh_.subscribe("/ni_out", 10, &interface::infoNiCallback, this); //type not known yet
-
-	// ackermann_to_ni_pub_ = nh_.advertise<ackermann_msgs::AckermannDrive>("/ni_in", 10);
+	ackermann_sub_= nh_.subscribe("/stellgroessen_safe", 10, &interface::CASCallback, this);
+	ackermann_sub_= nh_.subscribe("/ni_out", 10, &interface::infoNiCallback, this); 
+	emergency_stop_sub_= nh_.subscribe("state",10, &interface::stop_Callback, this); //type not known yet
+	// dspace_v_left_sub_= nh_.subscribe("/dspace_out", &interface::velocity_left_tkp_Callback, this);
+	// dspace_v_right_sub_= nh_.subscribe("/dspace_out", &interface::velocity_right_Callback, this);
+	ackermann_to_ni_pub_ = nh_.advertise<ackermann_msgs::AckermannDrive>("/ni_in", 10);
+	right_wheel_pub_ = nh_.advertise<std_msgs::Bool>("vcu_stop", 10);
+	steering_pub_ = nh_.advertise<std_msgs::Float64>("steer_angle", 10);
+	left_wheel_pub_ = nh_.advertise<std_msgs::Float64>("v_left", 10);
+	right_wheel_pub_ = nh_.advertise<std_msgs::Float64>("v_right", 10);
 }
 
 void interface::guardBoolCallback(const std_msgs::Bool::ConstPtr& msg){
 	// std::cout<< "I hear the safe bool"<<std::endl;
   	guard_bool_ = msg->data;
-  
 }
 
-void interface::ackCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg){ 
+void interface::stop_Callback(const arc_msgs::State::ConstPtr& msg){
+	stop_ = msg->stop;
+}
+
+void interface::CASCallback(const ackermann_msgs::AckermannDrive::ConstPtr& msg){ 
 	// std::cout<< "I have the input controls"<<std::endl;
  	control_input_ = *msg;
  	ROS_INFO("I heard: [%f]", control_input_.steering_angle);
@@ -37,13 +41,32 @@ void interface::infoNiCallback(const ackermann_msgs::AckermannDrive::ConstPtr& m
  	sensors_ni_= *msg;
 }
 
+// //Callback for dspace
+
+// void interface::velocity_left_dspace_Callback(const std_msgs::Float64::ConstPtr& msg){
+// 	v_left_dspace_ = *msg; //
+// }
+
+// void interface::velocity_right_dspace_Callback(const std_msgs::Float64::ConstPtr& msg){
+// 	v_right_dspace_ = *msg; //
+// }
+
+// void interface::steering_angle_dspace_Callback(const std_msgs::Float64::ConstPtr& msg){
+// 	steering_angle_dspace_ = *msg; //
+// }
+
+
 void interface::kill()
 {
 	ros::Rate loop_rate(10);
-	while(guard_bool_==0)
+	if(stop_==0)
 	{
 		ackermann_to_ni_pub_.publish(control_input_);
 		loop_rate.sleep();
+	}
+	else
+	{
+		ros::shutdown();
 	}
 }
 
